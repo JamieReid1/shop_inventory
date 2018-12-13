88989d5fcc3f488ed243d3069102865f4af79520
Jamie Reid <wjreid85.jr@gmail.com>
2018-12-13 16:11:03 +0000

    Create product class and add name with passing test.

--- models/specs/product_spec.rb
+++ models/specs/product_spec.rb
@@ -1,1 +1,31 @@
 
+require('minitest/autorun')
+require('minitest/rg')
+require('pry')
+require_relative('../product')
+
+
+class TestProduct < MiniTest::Test
+
+
+  def setup()
+
+    options = { 'id' => 1,
+                'name' => 'Superlite 17B',
+                'category' => 'helmets',
+                'description' => 'Carbon fibre shell with yoke and clamp neck dam.',
+                'buy_cost' => 3000,
+                'sell_cost' => 5000
+              }
+
+    @product = Product.new(options)
+
+  end
+
+
+  def test_product_name()
+    assert_equal('Superlite 17B', @product.name)
+  end
+
+
+end
