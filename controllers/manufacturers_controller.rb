b1fe7c5b08d9d24d11937c0a9913a06233710cbb
Jamie Reid <wjreid85.jr@gmail.com>
2018-12-16 11:26:36 +0000

    Add update product's manufacturer functionality.

--- controllers/manufacturers_controller.rb
+++ controllers/manufacturers_controller.rb
@@ -13,3 +13,14 @@ get '/manufacturer/:id' do
   @stock = Stock.find(params['id'])
   erb( :'manufacturers/update' )
 end
+
+post '/manufacturer/:id' do
+  stock = Stock.new(params)
+  manufacturer = stock.manufacturer
+  manufacturer.name = params['name']
+  manufacturer.address = params['address']
+  manufacturer.tel_no = params['tel_no']
+  manufacturer.rep_name = params['rep_name']
+  manufacturer.update()
+  redirect to ('/manufacturers')
+end
